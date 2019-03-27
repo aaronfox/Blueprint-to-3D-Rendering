b04b25cd272009d0676fef2442fa2f7a362905c1
Aaron Fox <aaron.m.fox3@gmail.com>
2019-03-27 12:33:15 -0400

    initial commit of FPV with basic blueprint rendering

---
+++ Assets/FirstPersonViewer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonViewer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("IN FPV");
+    }
+    [SerializeField] public float speed = 10.0f;
+    float horizontalMouseSpeed = 2.0f;
+    float verticalMouseSpeed = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftRight = Input.GetAxis("Horizontal") * speed;
+        float forwardBack = Input.GetAxis("Vertical") * speed;
+
+        leftRight *= Time.deltaTime;
+        forwardBack *= Time.deltaTime;
+
+        transform.Translate(leftRight, 0, 0);
+        transform.Translate(0, 0, forwardBack);
+
+        // Get the mouse delta. This is not in the range -1...1
+        //float h = horizontalMouseSpeed * Input.GetAxis("Mouse X");
+        //float v = verticalMouseSpeed * Input.GetAxis("Mouse Y");
+
+        //transform.Rotate(v, h, 0);
+
+    }
+}
---
+++ Assets/Scripts/ReadImage.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadImage : MonoBehaviour
+{
+    [SerializeField] private Texture2D[] images;
+    private Texture2D image;
+
+    [SerializeField] private GameObject wallObject;
+    [SerializeField] private GameObject groundObject;
+
+    // For rezising images:
+    // https://resizeimage.net/
+    // TODO for tomorrow: create prefabs to instantiate and follow along video at 18:00 at https://www.youtube.com/watch?v=ae6mW74FZSU
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.DateTime before = System.DateTime.Now;
+        image = images[4];
+        Color[] pix = image.GetPixels();
+
+        int worldX = image.width;
+        int worldZ = image.height;
+
+        Vector3[] spawnPositions = new Vector3[pix.Length];
+        Vector3 startingSpawnPosition = new Vector3(-Mathf.Round(worldX / 2), 0, -Mathf.Round(worldZ / 2));
+        Vector3 currentSpawnPosition = startingSpawnPosition;
+
+        int counter = 0;
+
+        for (int z = 0; z < worldZ; z++)
+        {
+            for (int x = 0; x < worldX; x++)
+            {
+                spawnPositions[counter] = currentSpawnPosition;
+                counter++;
+                currentSpawnPosition.x++;
+            }
+
+            currentSpawnPosition.x = startingSpawnPosition.x;
+            currentSpawnPosition.z++;
+        }
+
+        counter = 0;
+        //int whitePix = 0;
+        //int notWhitePix = 0;
+        Color white = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        foreach (Vector3 pos in spawnPositions)
+        {
+            Color c = pix[counter];
+
+            if(c != white)
+            {
+                //Debug.Log("Not white");
+                Instantiate(wallObject, pos, Quaternion.identity);
+                //notWhitePix++;
+            }
+            else
+            {
+                //Debug.Log("White");
+                //whitePix++;
+                // Not instantiating groundObject for performance purposes
+                //Instantiate(groundObject, pos, Quaternion.identity);
+
+
+            }
+            counter++;
+        }
+        System.TimeSpan duration = System.DateTime.Now.Subtract(before);
+        Debug.Log("Generation of map took " + duration.Milliseconds + " milliseconds");
+        //Debug.Log("Found " + whitePix + " white pixels");
+        //Debug.Log("Found " + notWhitePix + " non-white pixels");
+
+        //foreach (Color c in pix)
+        //{
+
+        //}
+        //Color white = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        //for (int i = 0; i < pix.Length; i++)
+        //{
+        //    //Debug.Log("i == " + i);
+        //    if (pix[i] == Color.white)
+        //    {
+        //        Debug.Log("Pixel is white!");
+        //    }
+        //    else
+        //    {
+
+        //        if (pix[i] != white)
+        //        {
+        //            Debug.Log("!!pix[i] == " + pix[i]);
+        //        }
+        //    }
+        //}
+        //Debug.Log("pix.Length == " + pix.Length);
+    }
+}
